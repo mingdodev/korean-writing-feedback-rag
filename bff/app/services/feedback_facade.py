003ce093a6c039edb3cec6693cbe9b672e87629b
mingdodev <mingdo.dev@gmail.com>
2025-11-20 01:48:18 +0900

    내부 서비스들을 조합하는 FeedbackFacade 레이어 구현

--- bff/app/services/feedback_facade.py
+++ bff/app/services/feedback_facade.py
@@ -2,7 +2,7 @@ from .context_service import ContextService
 from .grammar_service import GrammarService
 from .sentence_service import SentenceService
 from ..schemas.feedback_request import FeedbackRequest
-from ..schemas.feedback_response import FeedbackResponse, ContextFeedback, Sentence
+from ..schemas.feedback_response import FeedbackResponse, ContextFeedback, GrammarFeedback, Sentence
 
 class FeedbackFacade:
     def __init__(
@@ -16,27 +16,31 @@ class FeedbackFacade:
         self.sentence_service = sentence_service
 
     async def create_feedback(self, request: FeedbackRequest) -> FeedbackResponse:
-        # 1. 문맥 피드백 서비스
-        # request.title, request.contents로 LLM API 호출 -> context_feedback
+        # 1. 문맥 피드백
+        context_feedback: ContextFeedback = await self.context_service.create_context_feedback(
+            title=request.title,
+            contents=request.contents,
+        )
 
-        # 2. 문법 교정 서비스
+        # 2. 문장 분할
+        sentences = self.sentence_service.split_into_sentences(request.contents)
 
-        # requst.contents를 문장 단위로 분할 -> list[sentence]
-        # sentencd: id, text
+        # 3. 오류를 포함한 문장 태깅
+        sentences = self.sentence_service.tag_error_sentences_by_konlpy(sentences)
 
-        # list[sentence] 각각에 대해 문장의 오류 포함 여부 판별 -> list[error_sentence]
-        # error_sentence: id, text, grammar_feedback=None (아직)
+        # 4. 문법 교정: 오류 문장만 LLM 호출
+        
+        for sentence in sentences:
+            if not sentence.is_error_candidate:
+                continue
 
-        # list[error_sentence] 각각에 대해 문법 교정 서비스 호출 -> list[error_sentence]
-        # error_sentence: id, text, grammar_feedback=LLMGrammarFeedback(...)
-        # LLMGrammarFeedback: origin_sentence, corrected_sentence, feedback: str
+            feedback: GrammarFeedback = await self.grammar_service.attach_grammar_feedback([sentence])
+            if feedback:
+                sentence.grammar_feedback = feedback
 
-        # list[sentence]를 순회하며 GrammarFeedback 매핑 -> list[sentence] (실제 API response)
-        # 예) {sentence_id=1, original_sentence=..., grammar_feedback=None} 
-        #   {sentence_id=2, original_sentence=..., grammar_feedback=GrammarFeedback(...)}
-        # GrammarFeedback: corrected_sentence, feedback
 
+        # 5. 최종 응답 조립
         return FeedbackResponse(
-            context_feedback=ContextFeedback(),
-            sentences=[],
+            context_feedback=context_feedback,
+            sentences=sentences,
         )
