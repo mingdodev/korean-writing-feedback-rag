24fdedc0769bdca9f9e6beee28ec826414815173
mingdodev <mingdo.dev@gmail.com>
2025-11-23 17:33:14 +0900

    LLM Client 의존성 주입
    - Clova LLM

--- bff/app/core/dependencies.py
+++ bff/app/core/dependencies.py
@@ -4,14 +4,17 @@ from kafka import KafkaProducer
 
 from ..clients.context_llm_client import ContextLLMClient
 from ..clients.grammar_llm_client import GrammarLLMClient
+from ..llm.clova_client import ClovaStudioClient
 from ..services.context_service import ContextService
 from ..services.grammar_service import GrammarService
 from ..services.sentence_service import SentenceService
 from ..services.feedback_facade import FeedbackFacade
 from ..services.collect_event_publisher import CollectEventPublisher
 
-context_client = ContextLLMClient()
-grammar_client = GrammarLLMClient()
+llm_client = ClovaStudioClient()
+
+context_client = ContextLLMClient(llm_client)
+grammar_client = GrammarLLMClient(llm_client)
 
 context_service = ContextService(context_client)
 grammar_service = GrammarService(grammar_client)
